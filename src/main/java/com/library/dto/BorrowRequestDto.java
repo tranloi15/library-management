1b07b6818a9c1e1da111ee405f25146a079d336b
hoaminhh <nguyenminhhoa.210.2006@gmail.com>
2026-09-21 20:21:00 +0700

    cap nhat tinh nang

---
+++ src/main/java/com/library/dto/BorrowRequestDto.java
@@ -0,0 +1,42 @@
+package com.library.dto;
+
+import java.time.LocalDate;
+
+public class BorrowRequestDto {
+    private Long userId;
+    private Long bookId;
+    private LocalDate borrowDate;
+    private LocalDate dueDate;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDate borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
